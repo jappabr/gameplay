debff37ebf17b536802a60ef737f06e0228e3ff1
JappaBR <guilherme.dion@gmail.com>
2021-08-03 17:24:08 -0300

    Add component "Avatar"

---
+++ src/components/Avatar/styles.ts
@@ -0,0 +1,17 @@
+import { StyleSheet } from "react-native";
+import { theme } from "../../global/styles/theme";
+
+export const styles = StyleSheet.create({
+  container:{
+    width: 50,
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginRight: 22,
+  },
+  avatar:{
+    width: 48,
+    height: 48,
+    borderRadius: 8,
+  }
+});
