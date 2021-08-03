40aec96b1a0421b876c3ab5897b36370356ed39c
JappaBR <guilherme.dion@gmail.com>
2021-08-03 17:23:12 -0300

    Update App.tsx
    New changes

--- App.tsx
+++ App.tsx
@@ -1,12 +1,12 @@
-import React from "react";
+import React from 'react';
 import { StatusBar } from 'react-native';
+import { Inter_400Regular, Inter_500Medium} from '@expo-google-fonts/inter';
+import { Rajdhani_500Medium, Rajdhani_700Bold } from '@expo-google-fonts/rajdhani';
+import AppLoading from 'expo-app-loading';
 import { useFonts } from 'expo-font';
-import { Inter_400Regular, Inter_500Medium} from '@expo-google-fonts/inter'
-import { Rajdhani_500Medium, Rajdhani_700Bold } from '@expo-google-fonts/rajdhani'
-import AppLoading from "expo-app-loading";
 
-import { SignIn } from "./src/screens/SignIn";
-
+import { Background } from './src/components/Background';
+import { Home } from './src/screens/Home';
 
 export default function App(){
   const [fontsLoaded] = useFonts({
@@ -21,14 +21,13 @@ export default function App(){
   }
 
   return(
-    <>
-      <StatusBar
-      barStyle="light-content"
-      backgroundColor="transparent"
-      translucent
+    <Background>
+      <StatusBar 
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
       />
-
-    <SignIn />
-    </>
+      <Home/>
+    </Background>
   );
 }
