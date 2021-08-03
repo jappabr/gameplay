8c85d5b8ba3c7161d8e7d4e9dec93209fb25d664
JappaBR <guilherme.dion@gmail.com>
2021-08-03 17:26:01 -0300

    Update SignIn - styles.ts

--- src/screens/SignIn/styles.ts
+++ src/screens/SignIn/styles.ts
@@ -1,11 +1,10 @@
 import { StyleSheet } from "react-native";
-import { theme } from "../../global/styles.ts/theme";
+import { theme } from "../../global/styles/theme";
 export const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: theme.colors.secondary100,
     },
     image:{
         width:'100%',
