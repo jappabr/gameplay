bf54f77cf0de6c46a1833261b882487a74c17734
JappaBR <guilherme.dion@gmail.com>
2021-08-06 18:12:19 -0300

    Update styles.ts

--- src/components/Avatar/styles.ts
+++ src/components/Avatar/styles.ts
@@ -1,17 +1,17 @@
-import { StyleSheet } from "react-native";
-import { theme } from "../../global/styles/theme";
+import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
-  container:{
-    width: 50,
-    height: 50,
+  container: {
+    width: 49,
+    height: 49,
+    borderRadius: 8,
     alignItems: 'center',
     justifyContent: 'center',
-    marginRight: 22,
+    marginRight: 22
   },
-  avatar:{
-    width: 48,
-    height: 48,
-    borderRadius: 8,
+  avatar: {
+    width: 46,
+    height: 46,
+    borderRadius: 8
   }
 });
