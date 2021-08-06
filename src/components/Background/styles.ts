03ec9608ecfa56828b0e070de67681a6a5fd8ad7
JappaBR <guilherme.dion@gmail.com>
2021-08-06 18:12:34 -0300

    Update styles.ts

--- src/components/Background/styles.ts
+++ src/components/Background/styles.ts
@@ -1,7 +1,7 @@
-import { StyleSheet } from 'react-native';
+import { Dimensions, StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    }
+  container: {
+    height: Dimensions.get('window').height
+  }
 });
